23a227d3cc05c7661321e7451d753adb0e8cd477
Max Savitsky <maxsavitsky2015@gmail.com>
2021-03-19 14:26:13 +0300

    version increment

--- src/main/java/com/maxsavteam/calculator/Calculator.java
+++ src/main/java/com/maxsavteam/calculator/Calculator.java
@@ -26,6 +26,8 @@ public class Calculator {
     public static final String FI_SIGN = "\u03C6";
     public static final String E_SIGN = "\u0190";
 
+    public static final String VERSION = "1.0-rc2";
+
     private final TreeBuilder builder;
     private final CalculatorExpressionTokenizer mExpressionTokenizer;
     private final CalculatorExpressionBracketsChecker mBracketsChecker;
