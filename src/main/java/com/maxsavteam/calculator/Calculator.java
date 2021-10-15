823f427503d8556eae237cf37bc2737f0d7e5268
Max Savitsky <maxsavitsky2015@gmail.com>
2021-10-15 19:04:44 +0300

    build: change version to 1.8.0

--- src/main/java/com/maxsavteam/calculator/Calculator.java
+++ src/main/java/com/maxsavteam/calculator/Calculator.java
@@ -51,7 +51,7 @@ public class Calculator {
     public static final String FI_SIGN = "\u03C6";
     public static final String E_SIGN = "\u0190";
 
-    public static final String VERSION = "1.7.2";
+    public static final String VERSION = "1.8.0";
 
     private final TreeBuilder builder;
     private final CalculatorExpressionTokenizer mExpressionTokenizer;
