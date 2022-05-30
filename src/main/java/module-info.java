59d189ec0b809506c4202ab5e687cab548ec5523
Max Savitsky <maxsavitsky2015@gmail.com>
2022-05-30 22:28:30 +0300

    feat: add module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module com.maxsavteam.calculator {
+	exports com.maxsavteam.calculator;
+	opens com.maxsavteam.calculator;
+
+	requires ch.obermuhlner.math.big;
+	requires org.jetbrains.annotations;
+}
