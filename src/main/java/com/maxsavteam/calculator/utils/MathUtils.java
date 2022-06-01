ea01554ddb6efcc04abd1f5dd0c9c6ce83ab85dd
Max Savitsky <maxsavitsky2015@gmail.com>
2022-06-01 14:13:16 +0300

    fix: fix tan and cot

--- src/main/java/com/maxsavteam/calculator/utils/MathUtils.java
+++ src/main/java/com/maxsavteam/calculator/utils/MathUtils.java
@@ -110,7 +110,7 @@ public class MathUtils {
 	}
 
 	public static BigDecimal tan(BigDecimal x) {
-		if(cos(x).signum() == 0)
+		if(cos(x).setScale(roundScale, RoundingMode.HALF_UP).signum() == 0)
 			throw new CalculationException(CalculationException.INVALID_VALUE_FOR_TANGENT_OR_COTANGENT);
 		return BigDecimalMath.tan(x, new MathContext(HIGH_ROUND_SCALE));
 	}
@@ -120,7 +120,7 @@ public class MathUtils {
 	}
 
 	public static BigDecimal cot(BigDecimal x) {
-		if(sin(x).signum() == 0)
+		if(sin(x).setScale(roundScale, RoundingMode.HALF_UP).signum() == 0)
 			throw new CalculationException(CalculationException.INVALID_VALUE_FOR_TANGENT_OR_COTANGENT);
 		return BigDecimalMath.cot(x, new MathContext(HIGH_ROUND_SCALE));
 	}
