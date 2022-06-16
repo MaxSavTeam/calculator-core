39718a65598c49d05bfec84ecfbd5eeff8c8429f
Max Savitsky <maxsavitsky2015@gmail.com>
2022-06-16 23:38:04 +0300

    refactor: extract 'core' module

--- core/src/main/java/com/maxsavteam/calculator/Calculator.java
+++ core/src/main/java/com/maxsavteam/calculator/Calculator.java
@@ -1,6 +1,5 @@
 /*
- * Copyright (C) 2021 MaxSav Team
- *
+ * Copyright (C) 2022 MaxSav Team
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
  * Software Foundation, either version 3 of the License, or (at your option)
